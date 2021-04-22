0fad0fcb51b3b43ef34e5de9b53adac9a4833b29
Asus <It18023992@my.sliit.lk>
2021-04-23 01:25:34 +0530

    insert query

--- PaymentAndFund/src/com/PaymentAndFundService.java
+++ PaymentAndFund/src/com/PaymentAndFundService.java
@@ -1,26 +1,11 @@
 package com;
 
-import model.PaymentAndFund; 
 
-//For REST Service
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-//For JSON
-import com.google.gson.*;
-//For XML 
-import org.jsoup.*;
-import org.jsoup.parser.*;
-import org.jsoup.nodes.Document;
 
-@Path("/Payment")
+
 
 public class PaymentAndFundService {
-	PaymentAndFund PayObj = new PaymentAndFund();
-
-	@GET
-	@Path("/")
-	@Produces(MediaType.TEXT_HTML)
-	public String readPayment() {
-		return PayObj.readPayment();
+	
 	}
-}
+	
+
--- PaymentAndFund/src/model/PaymentAndFund.java
+++ PaymentAndFund/src/model/PaymentAndFund.java
@@ -22,5 +22,67 @@ public class PaymentAndFund {
 		}
 		return con;
 	}
+	
+	public String insertPayment(String pId, String prName, String pyDate, String pyAmount) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			// create a prepared statement
+			String query = " insert into paymentservice(`pId`,`prName`, `pyDate`,`pyAmount`)" + " values (?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, prName);
+			preparedStmt.setString(3, pyDate);
+			preparedStmt.setString(4, pyAmount);
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+		} catch (Exception e) {
+			output = "Error while inserting the Payment.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
+
+	public String readPayment() {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border=\"1\"><tr><th>Payment ID</th><th>Product Name</th><th>Date</th><th>Amount</th></tr>";
+			String query = "select * from paymentservice";
+			Statement stmt = (Statement) con.createStatement();
+			ResultSet rs = ((java.sql.Statement) stmt).executeQuery(query);
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String pId = Integer.toString(rs.getInt("pId"));
+				String prName = rs.getString("prName");
+				String pyDate = rs.getString("pyDate");
+				String pyAmount = rs.getString("pyAmount");
+
+				output += "<tr><td>" + pId + "</td>";
+				output += "<td>" + prName + "</td>";
+				output += "<td>" + pyDate + "</td>";
+				output += "<td>" + pyAmount + "</td>";
+
+			}
+			con.close();
+
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the Payment.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 
 }

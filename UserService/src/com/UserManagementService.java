b8d2abac60910a18da82fab2c44b2c0e168f4312
achiniPR <achinirupasingheapr@gmail.com>
2021-04-24 22:13:52 +0530

    Initial Commit of User Service after Name Refactoring

---
+++ UserService/src/com/UserManagementService.java
@@ -0,0 +1,91 @@
+package com;
+
+import model.UserManagement;
+
+//For Rest Services
+import javax.ws.rs.*; 
+import javax.ws.rs.core.MediaType; 
+
+//For JSON
+import com.google.gson.*; 
+
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+@Path("/user-management") 
+
+public class UserManagementService 
+	{ 
+		UserManagement UserManagementObj = new UserManagement();
+		
+		@GET
+		@Path("/") 
+		@Produces(MediaType.TEXT_HTML)
+		
+		public String readUserManagement() 
+		{ 
+			return UserManagementObj.readUserManagement();
+		}
+		
+		@GET
+		@Path("/{user_ID}") 
+		@Produces(MediaType.TEXT_HTML)
+		
+		public String readUserManagement(@PathParam("user_ID") String userid) 
+		{ 
+			return UserManagementObj.readUserByID(userid);
+		}
+		
+		@POST
+		@Path("/") 
+		@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+		@Produces(MediaType.TEXT_PLAIN) 
+		
+		public String insertUserManagement(@FormParam("username") String username, 
+									@FormParam("email") String email, 
+									@FormParam("contactNo") String contactNo, 
+									@FormParam("address") String address) 
+		{ 
+		 String output = UserManagementObj.insertUserManagement(username, email, contactNo, address); 
+		return output; 
+		}
+		
+		@PUT
+		@Path("/") 
+		@Consumes(MediaType.APPLICATION_JSON) 
+		@Produces(MediaType.TEXT_PLAIN) 
+		public String updateUserManagement(String userData) 
+		
+		{ 
+			//Convert the input string to a JSON object 
+			JsonObject userObject = new JsonParser().parse(userData).getAsJsonObject();
+			
+			//Read the values from the JSON object
+			String userID = userObject.get("userID").getAsString(); 
+			String username = userObject.get("username").getAsString(); 
+			String email = userObject.get("email").getAsString(); 
+			int contactNo = userObject.get("contactNo").getAsInt(); 
+			String address = userObject.get("address").getAsString(); 
+			String output = UserManagementObj.updateUserManagement(userID, username, email, contactNo, address); 
+		return output; 
+		}
+		
+		@DELETE
+		@Path("/") 
+		@Consumes(MediaType.APPLICATION_XML) 
+		@Produces(MediaType.TEXT_PLAIN) 
+		public String deleteUserManagement(String userData) 
+		{ 
+		//Convert the input string to an XML document
+		 Document doc = Jsoup.parse(userData, "", Parser.xmlParser()); 
+		 
+		//Read the value from the element <itemID>
+		 String userID = doc.select("userID").text();
+		 
+		 String output = UserManagementObj.deleteUserManagement(userID); 
+		return output; 
+		}
+		
+		
+}

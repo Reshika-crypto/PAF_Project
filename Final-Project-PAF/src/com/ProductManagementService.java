a924bfc9334dc556f7c4e1d1094affafa94e33b3
Reshika-crypto <reshikarajapaksha9@gmail.com>
2021-04-24 12:55:31 +0530

    read product function

--- Final-Project-PAF/src/com/ProductManagementService.java
+++ Final-Project-PAF/src/com/ProductManagementService.java
@@ -1,3 +1,77 @@
+package com;
+import model.ProductManagement;
 
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+@Path("/ProductManagement")
+
+public class ProductManagementService 
+	{
+		ProductManagement productObj = new ProductManagement();
+		
+		@GET
+		@Path("/")
+		@Produces(MediaType.TEXT_HTML)
+		public String readItems()
+		 {
+		 return productObj.readProduct();
+		 }
+		@POST
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+		@Produces(MediaType.TEXT_PLAIN)
+		
+		public String insertProduct(@FormParam("product_id") String product_id,
+		 @FormParam("product_name") String product_name,
+		 @FormParam("product_description") String product_description,
+		 @FormParam("product_quality") String product_quality,
+		 @FormParam("product_price") String product_price)
+		{
+		 String output = productObj.insertProduct(product_id, product_name, product_description, product_quality, product_price);
+		 return output;
+		}
+		
+		@PUT
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_JSON)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String updateProduct(String productData)
+		{
+		//Convert the input string to a JSON object
+		 JsonObject productObject = new JsonParser().parse(productData).getAsJsonObject();
+		//Read the values from the JSON object
+		 String product_id = productObject.get("product_id").getAsString();
+		 String product_name = productObject.get("product_name").getAsString();
+		 String product_description = productObject.get("product_description").getAsString();
+		 String product_quality = productObject.get("product_quality").getAsString();
+		 String product_price = productObject.get("product_price").getAsString();
+		 String output = productObj.updateProduct(product_id, product_name, product_description, product_quality, product_price);
+		return output;
+		}
+
+
+		@DELETE
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_XML)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String deleteProduct(String productData)
+		{
+		//Convert the input string to an XML document
+		 Document doc = Jsoup.parse(productData, "", Parser.xmlParser());
+
+		//Read the value from the element <itemID>
+		 String product_id = doc.select("product_id").text();
+		 System.out.println(product_id);
+		 String output = productObj.deleteProduct(product_id);
+		return output;
+		}
+}
 
 
--- Final-Project-PAF/src/model/ProductManagement.java
+++ Final-Project-PAF/src/model/ProductManagement.java
@@ -39,11 +39,11 @@ public class ProductManagement
 				PreparedStatement preparedStmt = con.prepareStatement(query);
 	 
 				// binding values
-				preparedStmt.setString(0, pID);
-				preparedStmt.setString(1, pname);
-				preparedStmt.setString(2, pdesc);
-				preparedStmt.setString(3, pQuality);
-				preparedStmt.setDouble(4, Double.parseDouble(price));
+				preparedStmt.setString(1, pID);
+				preparedStmt.setString(2, pname);
+				preparedStmt.setString(3, pdesc);
+				preparedStmt.setString(4, pQuality);
+				preparedStmt.setDouble(5, Double.parseDouble(price));
 	
 				// execute the statement
 				preparedStmt.execute();
@@ -60,6 +60,67 @@ public class ProductManagement
 			return output;
 		}
 	
+	public String readProduct()
+	{
+			 
+		String output = "";
+			 
+		try
+		{
+			 
+			
+			Connection con = connect();
+			
+			if (con == null)
+			{return "Error while connecting to the database for reading."; }
+			
+			// Prepare the html table to be displayed
+			output = "<table border='1'><tr><th>Product ID</th><th>Product Name</th>" +
+					"<th>Product Description</th>" +
+					"<th>Product Quality</th>" +
+					"<th>Product Price</th>" +
+					"<th>Update</th><th>Remove</th></tr>";
+
+			String query = "select * from product";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			 
+			// iterate through the rows in the result set
+			while (rs.next())
+			{
+								
+				String product_id = rs.getString("product_id");
+				String product_name = rs.getString("product_name");
+				String product_description = rs.getString("product_description");
+				String product_quality = rs.getString("product_quality");
+				String product_price = Double.toString(rs.getDouble("product_price"));
+			 
+				// Add into the html table
+				output += "<tr><td>" + product_id + "</td>";
+				output += "<td>" + product_name + "</td>";
+				output += "<td>" + product_description + "</td>";
+				output += "<td>" + product_quality + "</td>";
+				output += "<td>" + product_price + "</td>";
+			 
+				// buttons
+				output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"
+							+ "<td><form method='post' action='product.jsp'>"
+							+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+							+ "<input name='productID' type='hidden' value='" + product_id
+							+ "'>" + "</form></td></tr>";
+			 }
+			 con.close();
+			 
+			 // Complete the html table
+			 output += "</table>";
+		}
+		catch (Exception e)
+		{
+			 output = "Error while reading the product.";
+			 System.err.println(e.getMessage());
+		}
+		return output;
+	}
 	
 		
 } 
